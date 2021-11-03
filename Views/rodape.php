86178bdab95c0bdb857d2cd74eba124fd49403d1
sthefanyCaires <sthefanyacc2@hotmail.com>
2021-11-03 12:53:14 -0300

    alterações no rodapé

--- Views/rodape.php
+++ Views/rodape.php
@@ -1,7 +1,7 @@
 <div class="row" id="rodape">
   <div class="col mx-0 px-0 py-3 bg-dark">
     <div class="text-center text-light" style="margin-bottom:0">
-      <p id="text-rodape">Feito com carinho por Danton & Sthefany</p>
+      <p id="text-rodape"><span class="titulo">[ </span>Feito com <i class="fas fa-heart"></i> por Danton & Sthefany<span class="titulo"> ]</span></p>
     </div>
   </div>
 </div>
